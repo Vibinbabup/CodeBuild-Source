4c6cff5e6d594d82981492628c632a825ed56cf0
Vibin <vibinbabup788@gmail.com>
2025-07-02 19:54:25 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
-print("Hello from index.py")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from index.py"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
