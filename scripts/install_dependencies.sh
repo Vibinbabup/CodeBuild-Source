2e8f1b4806cb754064035f603aafda51986fad29
Vibin <vibinbabup788@gmail.com>
2025-07-02 19:54:53 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-echo "🛠 Starting dependency installation..."
-
-# Update package list
-sudo apt-get update -y
-
-# Install Python and pip if not installed
-sudo apt-get install -y python3 python3-pip
-
-# Install Python dependencies from requirements.txt
+echo "Installing dependencies..."
+apt update -y
+apt install -y python3 python3-pip
 pip3 install -r /var/www/html/requirements.txt
-
-echo "✅ Dependencies installed successfully."
